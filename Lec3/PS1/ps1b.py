e1c276eba9d227f0f78ffcc928f8dc39fec7bde5
griever255 <jgargiulo95@gmail.com>
2021-03-29 19:31:58 -0400

    Finished ps1b

--- Lec3/PS1/ps1b.py
+++ Lec3/PS1/ps1b.py
@@ -22,15 +22,43 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     
     Returns: int, smallest number of eggs needed to make target weight
     """
-    # TODO: Your code here
-    pass
+    global calls 
+    calls += 1
+    minEggs = target_weight
+    if target_weight in egg_weights:
+        memo[target_weight] = 1
+        return 1
+    elif target_weight in memo.keys(): 
+        return memo[target_weight]
+    else:
+        for e in egg_weights:
+            if e <= target_weight:
+                numEggs = 1 + dp_make_weight(egg_weights, target_weight - e, memo)
+            if numEggs < minEggs:
+                minEggs = numEggs
+                memo[target_weight] = numEggs
+    return minEggs
+    
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
+    global calls
+    calls = 0
     egg_weights = (1, 5, 10, 25)
     n = 99
     print("Egg weights = (1, 5, 10, 25)")
     print("n = 99")
     print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
-    print("Actual output:", dp_make_weight(egg_weights, n))
+    print("Actual output:", dp_make_weight(egg_weights, n, {}))
+    print(f"Recursive calls = {calls}")
+    print()
+
+    calls = 0
+    egg_weights = (1, 5, 24, 25)
+    n = 72
+    print("Egg weights = (1, 5, 24, 25)")
+    print("n = 72")
+    print("Expected output: 3 (3 * 24 = 72)")
+    print("Actual output:", dp_make_weight(egg_weights, n, {}))
+    print(f"Recursive calls = {calls}")
     print()
